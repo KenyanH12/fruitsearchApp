ea801f440def46b0d702676125968a2c11cb4475
KenyanH12 <kenyanholman3@gmail.com>
2024-09-06 19:57:55 -0400

    unfinished product

---
+++ fruit.js
@@ -0,0 +1,39 @@
+const suggestions = document.getElementById('suggestions');
+const input = document.querySelector('#fruit');
+
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	let input = fruit.value;
+	if(input.length) {
+		results = fruit.filter((item) => {
+			return item.toLowerCase().includes(input.toLowerCase());
+		});
+	}
+
+	console.log(results);
+}
+
+
+function searchHandler(e) {
+	// TODO
+}
+
+function showSuggestions(results, inputVal) {
+
+
+	let content = results.map((item) => {
+		return '<li> $(item) </li>'
+	})
+	.join('');
+}
+
+function useSuggestion(e) {
+	// TODO
+}
+
+input.addEventListener('keyup', showSuggestions((content)))
+
+suggestions.addEventListener('click', useSuggestion);
